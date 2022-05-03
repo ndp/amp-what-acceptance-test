32c7d3112d34ac3d773057d148c9602596830d2d
Andy Peterson <andy@ndpsoftware.com>
2022-05-03 09:47:11 -0700

    minor wording consistency cleanup

--- steps/step_implementation.js
+++ steps/step_implementation.js
@@ -37,7 +37,6 @@ const {
         write,
       } = require('taiko')
 const assert = require('assert')
-const path = require('path')
 const expectedMatches = require('./expected-results.json');
 
 const headless = process.env.headless_chrome.toLowerCase() === 'true'
@@ -58,17 +57,17 @@ step('Emulate <device>', emulateDevice)
 
 step('Visit amp-what', async () => await goto(`${PROTOCOL}://${HOST}/`))
 
-step('Visit <arg0>', async path => await goto(`${PROTOCOL}://${HOST}${path}`))
+step('Visit <path>', async path => await goto(`${PROTOCOL}://${HOST}${path}`))
 
-step('Search for <query>', async query => {
-  await focus(textBox($TEXT_BOX))
+step('Search for <q>', async q => {
+  await focus(await textBox($TEXT_BOX))
   await clear()
-  await write(query, { delay: 1 })
+  await write(q, into(await textBox($TEXT_BOX)), { delay: 1 })
 })
 
-step('Type <arg0> slowly', async q => await write(q, { delay: 200 }))
+step('Type <q> slowly', async q => await write(q, { delay: 200 }))
 
-step('The page title is <expectedTitle>', async expectedTitle => assert.strictEqual(expectedTitle, await title()))
+step('The page title is <expected>', async expectedTitle => assert.strictEqual(expectedTitle, await title()))
 
 step('The page title is the generic page title', async () => {
   assert.strictEqual('Discover Unicode Character Entities & Symbols | Amp What', await title())
@@ -81,8 +80,8 @@ step('Search for lines <n> of <queries>', async (range, queries) => {
   const [first, last] = range.split('-')
   const qs = queries.split('\n').slice(first - 1, last).map(s => s.trim())
   for (const q of qs) {
-    await clear(textBox($TEXT_BOX), {navigationTimeout: 35000})
-    await write(q, into(textBox($TEXT_BOX)))
+    await clear(await textBox($TEXT_BOX))
+    await write(q, into(await textBox($TEXT_BOX)))
     assert.strictEqual(q, await textBox($TEXT_BOX).value())
 
     const match = expectedMatches[q.toLowerCase()] || q
@@ -120,30 +119,30 @@ step('Open details for result <nth>', async (nth) => {
   await focus(textBox($TEXT_BOX))
   await press('Tab')
   let i = parseInt(nth)
-  while(--i > 0) await press('ArrowRight')
+  while (--i > 0) await press('ArrowRight')
   await press('Enter')
 })
 
-step('Click link <arg0>', async text => await click(text))
+step('Click link <text>', async text => await click(text))
 
-step('Click number <arg0>', tap)
-step('Click the <arg0> character', async (char) => {
+step('Click number <text>', tap)
+step('Click the <char> character', async (char) => {
   await click(char)
   await press(' ')
 })
-step('Click the <arg0> symbol', async (char) => {
+step('Click the <char> symbol', async (char) => {
   await click(char)
   await press(' ')
 })
-step('Click the <arg0> message', tap)
+step('Click the <s> message', tap)
 
-step("Type the <arg0> key", async function (key) {
+step("Type the <key> key", async function (key) {
   await press(key)
 });
 
-step('Click link with title <arg0>', async title => await click(link({ title })))
+step('Click link with title <title>', async title => await click(link({ title })))
 
-step('I see the description <arg0>', async content => assert.ok(await text(content).exists()))
+step('I see the description <desc>', async desc => assert.ok(await text(desc).exists()))
 
 step('The path is now <path>', async path => {
   const expectedUrl = `${PROTOCOL}://${HOST}${path}`
@@ -155,17 +154,17 @@ step('The path is now <path>', async path => {
   assert.strictEqual(expectedUrl, await currentURL())
 })
 
-step('The query box contains <arg0>', async arg0 => {
+step('The query box contains <q>', async q => {
   const e = textBox({ placeholder: 'type to search' })
-  assert.strictEqual(arg0, await e.value())
+  assert.strictEqual(q, await e.value())
 })
 
 step('Click the symbol inside zoom', async () => click($(`#modal samp`)))
 
 step('Click the number inside zoom', async () => click($(`#modal span.num`)))
 
-step('The clipboard contains <text>', async expected => {
-  return
+step('The clipboard contains <expected>', async expected => {
+  return // no way to assert this, for now
   const text2 = await evaluate(() => navigator.clipboard)
   const text = await evaluate(async () => await navigator.clipboard.readText())
   console.log(

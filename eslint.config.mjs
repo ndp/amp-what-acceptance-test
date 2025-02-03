efca8795922ff5c705a35b60f388af3c20d05b86
Andy Peterson <andy@ndpsoftware.com>
2025-02-02 17:33:53 -0800

    add configs for typescript and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["./tests/**/*.ts"]},
+  {ignores: ['{.pnp*,test-results,playwright-report,node_modules,.yarn}']},
+  {ignores: ['eslint.config.mjs', 'playwright.config.ts']},
+  {languageOptions: {globals: globals.browser}},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      }
+    }
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-floating-promises': [2],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          'args': 'all',
+          'argsIgnorePattern': '^_',
+          'caughtErrors': 'all',
+          "caughtErrorsIgnorePattern": "^_",
+          "destructuredArrayIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "ignoreRestSiblings": true
+        }
+      ]
+    }
+  }
+];
+
